0b5e07f1164c5abbe75b5c3f8510f9f5f3c66420
lkb <cuglkb@163.com>
2019-02-18 16:29:18 +0800

    2019-02-18 第一题

---
+++ cpractice00/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k;
+    int count = 0;
+    for(i=1;i<5;i++){
+        for(j=1;j<5;j++){
+            for(k=1;k<5;k++){
+                if((i!=j)&&(i!=k)&&(j!=k)){
+                    printf("%d,%d,%d\n",i,j,k);
+                    count ++;
+                }
+            }
+        }
+    }
+
+    printf("Ò»¹²%d\n",count);
+    printf("Hello world!\n");
+    return 0;
+}
