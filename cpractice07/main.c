e4bf43e224b87bfb530d11bd7f222b431d470f4e
lkb <cuglkb@163.com>
2019-02-20 12:12:22 +0800

    2019-02-20 输出9*9口诀

---
+++ cpractice07/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Êä³ö9*9¿Ú¾÷ */
+int main()
+{
+    int i,j;
+    for(i=1;i<10;i++){
+        for(j=1;j<=i;j++){
+            printf("%d * %d = %-3d  ",i,j,i*j);
+        }
+        printf("\n");
+    }
+    printf("Hello world!\n");
+    return 0;
+}
