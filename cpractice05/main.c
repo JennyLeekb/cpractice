dc4974e9921951e35f194b6165967a3bd3b432c1
lkb <cuglkb@163.com>
2019-02-20 12:00:30 +0800

    2019-02-20 打印c

---
+++ cpractice05/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* 用*号输出字母C的图案 */
+int main()
+{
+    printf("  ***\n");
+    printf(" *\n");
+    printf(" *\n");
+    printf("  ***\n");
+    printf("Hello world!\n");
+    return 0;
+}
